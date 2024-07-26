5544b918293f92fc8d9e68df63df9d1f8175319f
lukacino <lukacino33@gmail.com>
2024-07-26 08:14:51 +0200

    Final commit, second part

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import axios from "axios";
+import ReactLoading from "react-loading";
 
 function App() {
   const [summary, setSummary] = useState("");
   const [summarized, setSummarized] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const handleSubmit = () => {
+    setIsLoading(true);
     axios
       .post("http://localhost:3000/api/summarize", { content: summary })
       .then((res) => {
         setSummarized(res.data);
+        setIsLoading(false);
       });
   };
   return (
@@ -27,7 +31,16 @@ function App() {
           <button
             onClick={handleSubmit}
             className='bg-blue-600 text-white p-2 pt-1 rounded-2xl mt-3'>
-            Summarize
+            {isLoading ? (
+              <ReactLoading
+                type='spin'
+                color='white'
+                height='45px'
+                width='50px'
+              />
+            ) : (
+              "Sumarize"
+            )}
           </button>
         </div>
         <div className=''>
